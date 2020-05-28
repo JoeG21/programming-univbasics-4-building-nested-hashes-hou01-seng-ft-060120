efa472d2f931aaf7773e8b6ba7ab6c68a525e468
Joe Gomez <jcggomez@live.com>
2020-05-28 18:28:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,11 +1,11 @@
 def base_hash
   hash = {railroads: {}}
-  # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
   hash
 end
 
 def monopoly_with_second_tier
+  hash = {railroads: {}}
+  hash
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
